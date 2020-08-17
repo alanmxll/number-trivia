a8eb0580dbd989c1f69b48e11b513b02bbe7948b
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-08-17 17:43:35 -0300

    bloc scaffolding & input conversion

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import './bloc.dart';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  @override
+  NumberTriviaState get initialState => InitialNumberTriviaState();
+
+  @override
+  Stream<NumberTriviaState> mapEventToState(
+    NumberTriviaEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
